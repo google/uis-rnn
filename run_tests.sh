aca5b535b182af99cd47a2bd1f5d7816105633c0
Quan Wang <quanw@google.com>
2018-10-18 14:14:03 -0400

    Add the first unit test, and a script to run tests

---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o errexit
+
+# Get project path.
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Add project modules to PYTHONPATH.
+if [[ "${PYTHONPATH}" != *"${PROJECT_PATH}"* ]]; then
+    export PYTHONPATH="${PYTHONPATH}:${PROJECT_PATH}"
+fi
+
+pushd ${PROJECT_PATH}
+
+# Run tests.
+for TEST_FILE in $(find tests -name "*_test.py"); do
+    echo "Running tests in ${TEST_FILE}"
+    python3 ${TEST_FILE}
+done
+echo "All tests passed!"
+
+popd
+
