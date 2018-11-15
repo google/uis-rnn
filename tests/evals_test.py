b70f1c325edc1df7f9d40616e6990102b6514155
Quan Wang <quanw@google.com>
2018-11-15 11:34:20 -0500

    Add the Hungarian algorithm based evaluation

--- tests/evals_test.py
+++ tests/evals_test.py
@@ -12,9 +12,59 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import random
 import unittest
 
 from model import evals
+
+
+class TestComputeSequenceMatchAccuracy(unittest.TestCase):
+
+  def test_get_list_inverse_index(self):
+    unique_ids = ['a', 3, 'abc']
+    expected = {
+      'a': 0,
+      3: 1,
+      'abc': 2
+    }
+    self.assertDictEqual(
+        expected,
+        evals.get_list_inverse_index(unique_ids))
+
+  def test_mismatched_sequences(self):
+    sequence1 = [0, 0, 1, 2, 2]
+    sequence2 = [3, 3, 4, 4, 1]
+    accuracy = evals.compute_sequence_match_accuracy(
+        sequence1, sequence2)
+    self.assertEqual(0.8, accuracy)
+
+  def test_equivalent_sequences(self):
+    sequence1 = [0, 0, 1, 2, 2]
+    sequence2 = [3, 3, 4, 1, 1]
+    accuracy = evals.compute_sequence_match_accuracy(
+        sequence1, sequence2)
+    self.assertEqual(1.0, accuracy)
+
+  def test_symmetry(self):
+    sequence1 = [1] * 10 + [2] * 20 + [3] * 30 + [4] * 40
+    sequence2 = [1] * 10 + [2] * 20 + [3] * 30 + [4] * 40
+    random.shuffle(sequence1)
+    random.shuffle(sequence2)
+    accuracy1 = evals.compute_sequence_match_accuracy(
+        sequence1, sequence2)
+    accuracy2 = evals.compute_sequence_match_accuracy(
+        sequence2, sequence1)
+    self.assertEqual(accuracy1, accuracy2)
+
+  def test_sequences_of_different_lengths(self):
+    sequence1 = [0, 0, 1, 2]
+    sequence2 = [3, 3, 4, 4, 1]
+    with self.assertRaises(Exception):
+      evals.compute_sequence_match_accuracy(sequence1, sequence2)
+
+  def test_empty_sequences(self):
+    with self.assertRaises(Exception):
+      evals.compute_sequence_match_accuracy([], [])
 
 
 class TestComputeApproximateSequenceAcuracy(unittest.TestCase):
