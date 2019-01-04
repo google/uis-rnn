24b5f3cc151709e927616529c0f94b20638fd8c6
Quan Wang <quanw@google.com>
2019-01-04 15:44:37 -0500

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -o errexit
 
+# This script requires these tools:
+# pip3 install --user --upgrade setuptools wheel
+# pip3 install --user --upgrade twine
+
 # Get project path.
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
