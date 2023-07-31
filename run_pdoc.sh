33f27686984bd1c03474ecef642d65b54c2e8f0d
Quan Wang <quanw@google.com>
2023-07-30 20:28:33 -0400

    Use pdoc instead of pdoc3

--- run_pdoc.sh
+++ run_pdoc.sh
@@ -8,12 +8,8 @@ pushd ${PROJECT_PATH}
 
 rm -r -f docs
 
-# This script requires pdoc3 at least 0.5.2:
-# pip3 install pdoc3
-python3 -m pdoc uisrnn --html --output-dir=docs
-
-mv docs/uisrnn/* docs/
-
-rm -r docs/uisrnn
+# This script requires pdoc:
+# pip3 install pdoc
+python3 -m pdoc uisrnn -o docs
 
 popd
